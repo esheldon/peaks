1ac3f63976dcd1ad05bc5637dc73e667532de86d
Erin Sheldon <erin.sheldon@gmail.com>
2019-09-06 12:53:53 -0400

    add unit tests

--- peaks/tests/test_finder.py
+++ peaks/tests/test_finder.py
@@ -2,7 +2,7 @@ import numpy as np
 import peaks
 
 
-def test_finder_smoke(seed=None, show=False):
+def test_finder_smoke(seed=8712, show=False):
     """
     make sure the thing runs
     """
@@ -14,8 +14,72 @@ def test_finder_smoke(seed=None, show=False):
 
     sim = peaks.sim.Sim(rng=rng, dims=dims, nobj=nobj)
 
-    im, object_data = sim.make_image()
+    image, object_data = sim.make_image()
+
+    kernel_fwhm_pix = sim.psf_fwhm/sim.scale
+    kernel = peaks.gauss_kernel(
+        fwhm=kernel_fwhm_pix,
+        dims=[25]*2,
+    )
+
+    finder = peaks.PeakFinder(
+        image=image,
+        kernel=kernel,
+        noise=sim.noise,
+    )
+    finder.go()
 
     if show:
-        import images
-        images.view(im)
+        peaks.vis.view_peaks(
+            image,
+            sim.noise,
+            finder.objects,
+        )
+
+
+def test_finder(seed=31415, show=False):
+    """
+    make sure we recover the right number of objects
+    in a grid test
+    """
+
+    rng = np.random.RandomState(seed)
+
+    dims = [50]*2
+    nobj = 9
+
+    sim = peaks.sim.GridSim(
+        rng=rng,
+        dims=dims,
+        nobj=nobj,
+        fwhm_range=(0, 0),
+        noise=0.0001,
+    )
+
+    image, object_data = sim.make_image()
+
+    kernel_fwhm_pix = sim.psf_fwhm/sim.scale
+    kernel = peaks.gauss_kernel(
+        fwhm=kernel_fwhm_pix,
+        dims=[25]*2,
+    )
+
+    finder = peaks.PeakFinder(
+        image=image,
+        kernel=kernel,
+        noise=sim.noise,
+    )
+    finder.go()
+
+
+    if show:
+        peaks.vis.view_peaks(
+            image=image,
+            noise=sim.noise,
+            objects=finder.objects,
+            show=True,
+        )
+
+    nfound = finder.objects.size
+    assert nfound == nobj, \
+        'expected %d objects, got %d' % (nobj, nfound)
